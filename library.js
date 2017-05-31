c6cf53caa007223d9b7f49cb863577db9707b98d
Chasteau <davidchasteau@gmail.com>
2017-05-31 21:32:06 +0000

    refactored the printPlaylists function

--- library.js
+++ library.js
@@ -29,6 +29,15 @@ var library = {
 // p01: Coding Music - 2 tracks
 // p02: Other Playlist - 1 tracks
 
+function returnPlaylists(currentPlaylist) {
+    var playlistId = currentPlaylist.id;
+    var playlistName = currentPlaylist.name;
+    var numberOfTracks = currentPlaylist.tracks.length;
+    return (playlistId + ": " + playlistName + " - " + numberOfTracks+ " tracks" );
+}
+
+
+
 var printPlaylists = function () {
 
   var libraryPlaylists =  library.playlists;
@@ -38,21 +47,23 @@ var printPlaylists = function () {
 
     if(libraryPlaylists.hasOwnProperty(currentPlaylist)) {
 
+      console.log(returnPlaylists(libraryPlaylists[currentPlaylist]));
+
     // access playlist object to get key name
-    var playlistId = libraryPlaylists[currentPlaylist].id;
+    // var playlistId = libraryPlaylists[currentPlaylist].id;
     //console.log(playlistId);
 
     // once we have id we need to get name of playlist
-    var playlistName = libraryPlaylists[currentPlaylist].name;
+    // var playlistName = libraryPlaylists[currentPlaylist].name;
     //console.log(playlistName);
 
     // then we need to get number of tracks (.length)
 
-    var numberOfTracks = libraryPlaylists[currentPlaylist].tracks.length;
+    // var numberOfTracks = libraryPlaylists[currentPlaylist].tracks.length;
     //console.log(numberOfTracks);
     // print results as one string.
 
-    console.log(playlistId + ": " + playlistName + " - " + numberOfTracks+ " tracks" );
+    // console.log(playlistId + ": " + playlistName + " - " + numberOfTracks+ " tracks" );
 
     }
 
@@ -61,6 +72,8 @@ var printPlaylists = function () {
 };
 
 printPlaylists();
+
+
 // prints a list of all tracks, in the form:
 // t01: Code Monkey by Jonathan Coulton (Thing a Week Three)
 // t02: Model View Controller by James Dempsey (WWDC 2003)
@@ -68,7 +81,34 @@ printPlaylists();
 
 var printTracks = function () {
 
+    var libraryTracks =  library.tracks;
+    for (var currentTrack in libraryTracks) {
+
+    //console.log(libraryPlaylist[currentTrack].name);
+
+    if(libraryTracks.hasOwnProperty(currentTrack)) {
+
+    // access playlist object to get key name
+    var trackId = libraryTracks[currentTrack].id;
+    //console.log(trackId);
+
+    // once we have id we need to get name of playlist
+    var trackName = libraryTracks[currentTrack].name;
+    //console.log(trackName);
+
+    var trackArtist = libraryTracks[currentTrack].artist;
+
+    var trackAlbum = libraryTracks[currentTrack].album;
+
+    console.log(trackId + ": " + trackName + " by " + trackArtist + " (" + trackAlbum +")" );
+
+    }
+
+  }
+
 }
+
+printTracks();
 
 
 // prints a list of tracks for a given playlist, in the form:
